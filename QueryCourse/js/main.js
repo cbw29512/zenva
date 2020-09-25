b8da9fb265620f8063bc89ce5c0e0ad790782595
ChrisWilson <divclass01@gmail.com>
2020-09-25 17:09:04 -0400

    jquery third child

---
+++ QueryCourse/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.solid').addClass('highlight');
+});
+
+// double agreater than gows down to second child element
+// $(document).ready(function(){
+//     $('#container >> (>>> third child)
+//     .nonsolid').addClass('highlight');
+// });
