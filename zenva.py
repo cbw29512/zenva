9ebbef7b325938451421a9465c9030c07b8c1271
ChrisWilson <divclass01@gmail.com>
2020-09-28 00:04:33 -0400

    learn python by making a game, lesson 13

--- zenva.py
+++ zenva.py
@@ -2,28 +2,28 @@
 # Class fields, methods, and constructors
 # Object instatiation
 
-class GameCharacter:
-    speed = 5
+# class GameCharacter:
+#     speed = 5
 
-    def __init__(self, name, width, height, x_pos, y_pos):
-        self.name = name
-        self.width = width
-        self.height = height
-        self.x_pos = x_pos
-        self.y_pos = y_pos
+#     def __init__(self, name, width, height, x_pos, y_pos):
+#         self.name = name
+#         self.width = width
+#         self.height = height
+#         self.x_pos = x_pos
+#         self.y_pos = y_pos
 
-    def move(self, by_x_amount, by_y_amount):
-        self.x_pos += by_x_amount
-        self.y_pos += by_y_amount
+#     def move(self, by_x_amount, by_y_amount):
+#         self.x_pos += by_x_amount
+#         self.y_pos += by_y_amount
 
 
-character_0 = GameCharacter('char_0', 50, 100, 100, 100)
-print(character_0.name)  # char_0
-character_0.name = 'char_1'
-print(character_0.name)  # char_1
-character_0.move(50, 100)
-print(character_0.x_pos)  # 150
-print(character_0.y_pos)  # 200
+# character_0 = GameCharacter('char_0', 50, 100, 100, 100)
+# print(character_0.name)  # char_0
+# character_0.name = 'char_1'
+# print(character_0.name)  # char_1
+# character_0.move(50, 100)
+# print(character_0.x_pos)  # 150
+# print(character_0.y_pos)  # 200
 
 #  character_0.name character_0.width character_0.height
 # character_0.x_pos character_0.y_pos
